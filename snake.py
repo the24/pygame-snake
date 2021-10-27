98ebe5a5684eb5152108deb71dd057a18145991f
the24 <19132.ma@gmail.com>
2021-10-27 21:32:18 +0200

    ajout de la queue du serpent

--- snake.py
+++ snake.py
@@ -1,4 +1,7 @@
-from typing import Tuple
+from typing import List, Tuple
+
+import pygame
+from pygame.rect import Rect
 from map import Map
 from object import Movable
 
@@ -11,6 +14,8 @@ class Snake(Movable):
         self.map = map
         self.dir = [0, 0]
         self.speed = 10
+        self.tail: List[Rect] = []
+        self.tail_lenght = 0
     
     def update(self):
         if self.dir == [0, 0]:
@@ -25,12 +30,22 @@ class Snake(Movable):
             self.x += self.dir[0] * 36
             self.y += self.dir[1] * 36
 
+            for i in range(len(self.tail) - 1):
+                self.tail[i] = self.tail[i + 1]
+            
+            self.tail[self.tail_lenght - 1] = pygame.Rect(self.prev_x, self.prev_y, 36, 36)
         else:
             delta_x = self.x - self.prev_x
             delta_y = self.y - self.prev_y
             
             self.rect.x += round(delta_x / 60 * self.speed)
             self.rect.y += round(delta_y / 60 * self.speed)
+    
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.color, self.rect)
+
+        for t in self.tail:
+            pygame.draw.rect(screen, self.color, t)
 
     def down(self):
         self.dir = [0, 1]
